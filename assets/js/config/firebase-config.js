906fb33a90619c9c41c699acbe8682cae77cabf8
Iskak Fatoni <iskakfatoni@gmail.com>
2026-08-02 19:52:15 +0700

    Update Firebase configuration credentials
    Updated Firebase configuration with new credentials.

--- assets/js/config/firebase-config.js
+++ assets/js/config/firebase-config.js
@@ -8,14 +8,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 / Ganti nilai string di bawah sesuai dengan nilai dari Firebase Console:
 / Project Settings -> General -> Your apps -> Web app -> SDK setup/configuration
 */
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "portal-iskakfatoni.firebaseapp.com",
-  projectId: "portal-iskakfatoni",
-  storageBucket: "portal-iskakfatoni.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890"
-};
+    const firebaseConfig = {
+      apiKey: "AIzaSyDhq4QoCn00GkfpKXSP8PnvSvEKa2yn0ic",
+      authDomain: "portal-iskakfatoni.firebaseapp.com",
+      projectId: "portal-iskakfatoni",
+      storageBucket: "portal-iskakfatoni.firebasestorage.app",
+      messagingSenderId: "367350728822",
+      appId: "1:367350728822:web:6b0c6fd4824102d8acf757",
+      measurementId: "G-0BNNKQQ24V"
+    };
 
 // Inisialisasi Firebase App Core
 const app = initializeApp(firebaseConfig);
