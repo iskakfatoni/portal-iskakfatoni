f1edc1c7b5ae63f66db55905ecbad2d25ed39b29
Iskak Fatoni <iskakfatoni@gmail.com>
2026-08-07 14:55:53 +0700

    Update Firebase configuration and initialization script

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,22 +1,23 @@
-// Configuration Firebase untuk portal-iskakfatoni
-// Ganti nilai di bawah ini sesuai dengan Firebase Console Anda:
-// Firebase Console > Project Settings > General > Your apps > SDK setup/configuration
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDhq4QoCn00GkfpKXSP8PnvSvEKa2yn0ic",
+    authDomain: "portal-iskakfatoni.firebaseapp.com",
+    projectId: "portal-iskakfatoni",
+    storageBucket: "portal-iskakfatoni.firebasestorage.app",
+    messagingSenderId: "367350728822",
+    appId: "1:367350728822:web:6b0c6fd4824102d8acf757",
+    measurementId: "G-0BNNKQQ24V"
+  };
 
-// Inisialisasi Firebase jika library CDN sudah dimuat
-if (typeof firebase !== 'undefined') {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-} else {
-  console.warn("Firebase SDK belum dimuat. Pastikan skrip Firebase CDN dipasang sebelum firebase-config.js.");
-}
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
